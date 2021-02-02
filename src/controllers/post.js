66eed61667557ffda122587479bc6da792e23514
fadhil <fadhildarma13@gmail.com>
2021-02-02 11:58:22 +0700

    sequelize crud rest api

---
+++ src/controllers/post.js
@@ -0,0 +1,140 @@
+const { Post } = require("../../models");
+
+exports.getPosts = async (req, res) => {
+  try {
+    const posts = await Post.findAll();
+
+    res.send({
+      message: "Posts Successfully Retrieved",
+      data: {
+        posts,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.getPostsById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const post = await Post.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!post) {
+      return res.send({
+        message: `Post with id ${id} Not Existed`,
+      });
+    }
+
+    res.send({
+      message: `Post with id ${id} Successfully Retrieved`,
+      data: {
+        post,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.addPost = async (req, res) => {
+  try {
+    const post = await Post.create(req.body);
+
+    res.send({
+      message: "Posts Successfully Created",
+      data: {
+        post,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.editPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const post = await Post.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!post) {
+      return res.send({
+        message: `Post with id ${id} Not Existed`,
+      });
+    }
+
+    await Post.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    const postUpdated = await Post.findOne({
+      where: {
+        id,
+      },
+    });
+
+    res.send({
+      message: `Posts with id ${id} Successfully Updated`,
+      data: {
+        post: postUpdated,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Post.destroy({
+      where: {
+        id,
+      },
+    });
+
+    res.send({
+      message: `Posts with id ${id} Successfully Deleted`,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+
+exports.functionName = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,9 +8,23 @@ const {
   deleteTodo,
 } = require("../controllers/todos");
 
+const {
+  getPosts,
+  getPostsById,
+  addPost,
+  editPost,
+  deletePost,
+} = require("../controllers/post");
+
 router.get("/todos", getTodos);
 router.post("/todo", addTodo);
 router.patch("/todo/:id", editTodo);
 router.delete("/todo/:id", deleteTodo);
+
+router.get("/posts", getPosts);
+router.get("/post/:id", getPostsById);
+router.post("/post", addPost);
+router.patch("/post/:id", editPost);
+router.delete("/post/:id", deletePost);
 
 module.exports = router;
